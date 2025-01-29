63e9311c787a46e7d50af06176a41aaa9ffef7e1
Ajay Pandhavale <aj40135821@wipro.com>
2025-01-29 15:10:39 +0530

    program for pelindrome number

---
+++ src/Pelindrome_Number.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pelindrome_Number{
+public static void main(String[] args){
+
+Scanner sc =new Scanner (System.in);
+
+System.out.println("enter number to check wether the number is pelindrome or not");
+int num=sc.nextInt();
+
+int temp=num,sum=0,digit;
+while (temp>0)
+{
+digit=temp%10;
+sum=(sum*10)+digit;
+temp/=10;
+
+}
+
+System.out.println("sum is  "+sum );
+
+if (sum==num)
+{
+System.out.println("the number "+num +" is pelindrome number");
+}
+else
+{
+System.out.println("the number "+num +" is not pelindrome number");
+
+}
+}
+
+
+}
